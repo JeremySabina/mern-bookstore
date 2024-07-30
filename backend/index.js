dc4cb87bf7b2e49d9619b5a49a89ef5bdb904516
Jeremy Sabina <jeremysabina@gmail.com>
2024-07-29 23:08:25 -0400

    react is so bloated. Its complete but needs lot of polish

--- backend/index.js
+++ backend/index.js
@@ -2,46 +2,33 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
-
+import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
 
 const app = express()
 
 // Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handling CORS policy
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// app.use(
+//     cors({
+//         origin: 'http://localhost:5555',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type']
+//     })
+// )
+
+
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send("<h1>Welcome to MERN Stack Tutorial</h1>")
 });
 
-//Route for Save a new book
 
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear'
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear
-        };
-
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book)
-
-    } catch (error) {
-        console.log(error.message)
-        response.status(500).send({message: error.message})
-    }
-})
+app.use('/books', booksRoute)
 
 mongoose
     .connect(mongoDBURL)
